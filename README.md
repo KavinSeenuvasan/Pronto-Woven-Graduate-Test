67cc736cc871bcbace8fe11912d71cd9c5ffa6ca
KavinSeenuvasan <kavinseemu@gmail.com>
2021-02-21 02:33:13 +1100

    Robot program

---
+++ README.md
@@ -0,0 +1,13 @@
+Pronto Woven: Digital Associate - Graduate Program Test
+
+Instructions to execute the code:
+1. Type the python command in the terminal to compile the program. My current python file is "Robot.py".
+2. Run the Python program.
+3. Give the input - a string of comma-separated commands enclosed in a single quotation marks, eg `F1,R1,B2,L1,B3`.
+4. Output - The minimum distance required to get back to the starting point.
+
+Design Decision:
+1. There are 4 directions in which the robot can turn. The robot is faced North by default.
+2. The code accepts any input values unless the given command is correct. You can turn the robot 90 degrees for N number of times such as "R2,R3,R4".
+3. The current position of the robot in x,y coordinates is obtained and displayed at each movements.
+4. Atlast the distance is calculated.
---
+++ Robot.py
@@ -0,0 +1,56 @@
+def getShortestDistance(inputValue):
+
+    #get x and y coordinates
+    pos = [0,0]
+
+    direction = ['N','E','S','W']
+    currentDirection = 0 #default current facing direction of robot is North
+
+    inputValue=inputValue.split(',')
+
+    #Move robot on valid moves
+    for inp in inputValue:
+        movement = inp[0]
+        val= int(inp[1])
+
+        #calculating the position of the robot according to the value given
+        if movement =='F':
+            if direction[currentDirection]=='N':
+                pos[1]=pos[1]+val
+
+            if direction[currentDirection]=='E':
+                pos[0]=pos[0]+val
+
+            if direction[currentDirection]=='S':
+                pos[1]=pos[1]-val
+
+            if direction[currentDirection]=='W':
+                pos[0]=pos[0]-val
+
+        if movement =='B':
+            if direction[currentDirection]=='N':
+                pos[1]=pos[1]-val
+
+            if direction[currentDirection]=='E':
+                pos[0]=pos[0]-val
+
+            if direction[currentDirection]=='S':
+                pos[1]=pos[1]+val
+
+            if direction[currentDirection]=='W':
+                pos[0]=pos[0]+val
+
+        if movement=='R':
+            currentDirection=(currentDirection+val)%4
+        if movement=='L':
+            currentDirection=abs(currentDirection-val)%4
+
+        #prints the current position of the robot as every move
+        print("x,y:", pos[0],pos[1])
+
+    #get distance
+    distance=abs(pos[0])+abs(pos[1])
+    print("Distance required to travel from ending position " + str(pos) +" to starting position [0,0] is " + str(distance))
+
+#give the input
+getShortestDistance(input("Enter the input: "))
